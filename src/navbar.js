234eb954766669a974db5f7d73265bc691b11f38
chenfeng198510 <chenfeng198510@gmail.com>
2022-05-09 20:37:16 -0700

    modify

--- src/navbar.js
+++ src/navbar.js
@@ -7,10 +7,10 @@ function Navbar() {
 <nav>
     <div className="logo">Lovely House</div>
     <ul className="nav-links" style={{transform: open ? "translateX (0px)" : "translateX(100)"}}> 
-        <li><a>Home</a></li>
-        <li><a>Product</a></li>
-        <li><a>About us</a></li>
-        <li><a>Contact</a></li>
+        <li><a href="#">Home</a></li>
+        <li>Product</li>
+        <li>About us</li>
+        <li>Contact</li>
         
     </ul>
     <i onClick={() => setOpen(!open)} className="fa-solid fa-house house"></i>
